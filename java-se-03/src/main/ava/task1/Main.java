1307fd58de613865116a555c227b28fd96d6608a
Slava Avramenok <_AVA@outlook.com>
2018-04-01 21:11:56 +0300

    03. Task 1. Add Main class with examples

---
+++ java-se-03/src/main/ava/task1/Main.java
@@ -0,0 +1,20 @@
+package main.ava.task1;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        CrazyLogger log = new CrazyLogger();
+        log.info("John has sold bike");
+        log.info("Mary bought the bike");
+        log.info("John Doe found his keys");
+        System.out.println(log.printLog());
+        System.out.println("_____________");
+        System.out.println(log.findByString("John"));
+        System.out.println("_____________");
+        System.out.println(log.findByString("bike"));
+        System.out.println("_____________");
+        System.out.println(log.findByDate(LocalDate.of(2018, 4, 1)));
+    }
+}
