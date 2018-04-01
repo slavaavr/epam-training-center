a07cf06454c4a34f63e033074ff287681dcb690b
Slava Avramenok <_AVA@outlook.com>
2018-04-01 21:05:32 +0300

    03. Task 1. Implements finding by date and time

--- java-se-03/src/main/ava/task1/CrazyLogger.java
+++ java-se-03/src/main/ava/task1/CrazyLogger.java
@@ -1,14 +1,18 @@
 package main.ava.task1;
 
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class CrazyLogger {
     private StringBuilder buff;
-    private static final String DATE_TIME_FORMAT = "dd-MM-YYYY : hh-mm - ";
+    private static final String TIME_FORMAT = "hh-mm";
+    private static final String DATE_FORMAT = "dd-MM-YYYY";
+    private static final String DATE_TIME_FORMAT = String.format("%s : %s - ",DATE_FORMAT, TIME_FORMAT);
 
     public CrazyLogger(){
         buff = new StringBuilder();
@@ -23,11 +27,32 @@ public class CrazyLogger {
 
     public String findByString(String string){
         String[] strings = buff.toString().split("\n");
-        return Arrays.stream(strings).filter(s -> s.contains(string)).collect(Collectors.joining("\n"));
+        return Arrays.stream(strings)
+                .filter(s -> s.contains(string))
+                .collect(Collectors.joining("\n"));
     }
+
+    public String findByDate(LocalDate date){
+        String[] strings = buff.toString().split("\n");
+        return Arrays.stream(strings)
+                .filter(s -> s.contains(date.format(DateTimeFormatter.ofPattern(DATE_FORMAT))))
+                .collect(Collectors.joining("\n"));
+    }
+
+    public String findByTime(LocalTime time){
+        String[] strings = buff.toString().split("\n");
+        return Arrays.stream(strings)
+                .filter(s -> s.contains(time.format(DateTimeFormatter.ofPattern(TIME_FORMAT))))
+                .collect(Collectors.joining("\n"));
+    }
+
 
     public void reset(){
         buff = new StringBuilder();
+    }
+
+    public String printLog(){
+        return buff.toString();
     }
 
 }
