9543930a2f9fbe94a7a1dcade011103133827c70
Slava Avramenok <_AVA@outlook.com>
2018-04-02 17:58:59 +0300

    04. Task 1. fix bug with keyword finding(old code has sought substrings instead of keywords)

--- java-se-04/src/main/java/ava/task1/KeywordStatisticsInFile.java
+++ java-se-04/src/main/java/ava/task1/KeywordStatisticsInFile.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class KeywordStatisticsInFile {
 
@@ -19,18 +21,21 @@ public class KeywordStatisticsInFile {
         return keywordCountList;
     }
 
-    public void parse(Path pathToFile, Path pathToKeywords){
+    public void parse(Path pathToFile, Path pathToKeywords) {
         StringBuilder keywords = new StringBuilder();
-        byte[] buff = new byte[1024];
-        try(BufferedInputStream brForKeywords = new BufferedInputStream(new FileInputStream(pathToKeywords.toString()));
-                BufferedInputStream brForFile = new BufferedInputStream(new FileInputStream(pathToFile.toString()))){
-            while(brForKeywords.read(buff) != -1){
+        byte[] buff;
+        try (BufferedInputStream brForKeywords = new BufferedInputStream(new FileInputStream(pathToKeywords.toString()));
+             BufferedInputStream brForFile = new BufferedInputStream(new FileInputStream(pathToFile.toString()))) {
+            buff = new byte[brForKeywords.available()];
+            while (brForKeywords.read(buff) != -1) {
                 keywords.append(new String(buff));
             }
             initKeywordCountList(keywords.toString());
-            while (brForFile.read(buff) != -1){
+            buff = new byte[brForFile.available()];
+            while (brForFile.read(buff) != -1) {
+                String string = new String(buff);
                 for (KeywordCount keywordCount : keywordCountList) {
-                    keywordCount.addCount(countSubstringWithinString(new String(buff),keywordCount.getKeyword()));
+                    keywordCount.addCount(countSubstringWithinString(string, keywordCount.getKeyword()));
                 }
             }
         } catch (IOException e) {
@@ -38,26 +43,30 @@ public class KeywordStatisticsInFile {
         }
     }
 
-    private void initKeywordCountList(String words){
+    private void initKeywordCountList(String words) {
         String[] strings = words.split("\r\n");
         for (String string : strings) {
-
             keywordCountList.add(new KeywordCount(string, 0));
-
         }
     }
 
-    private int countSubstringWithinString(String string, String substring){
+    private int countSubstringWithinString(String string, String substring) {
+        Pattern p = Pattern.compile(substring + "\\s");
+        Matcher m = p.matcher(string);
         int count = 0;
-        int offset = 0;
-        while((offset = string.indexOf(substring, offset))!=-1){
-            offset++;
-            count++;
+        while (m.find()) {
+            count += 1;
         }
+//        int count = 0;
+//        int lastIndex = 0;
+//        while ((lastIndex = string.indexOf(substring, lastIndex)) != -1) {
+//            lastIndex += substring.length() - 1;
+//            count++;
+//        }
         return count;
     }
 
-    public static class KeywordCount{
+    public static class KeywordCount {
         private String keyword;
         private int count;
 
