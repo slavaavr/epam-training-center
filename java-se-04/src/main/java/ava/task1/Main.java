8ee84eb30debce7ce458125c03159abc422fa283
Slava Avramenok <_AVA@outlook.com>
2018-04-02 17:59:26 +0300

    04. Task 1. Add main class with initial code

---
+++ java-se-04/src/main/java/ava/task1/Main.java
@@ -0,0 +1,13 @@
+package ava.task1;
+
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        KeywordStatisticsInFile statistics = new KeywordStatisticsInFile();
+        statistics.parse(Paths.get("java-se-04/src/main/java/ava/task1/fileForTest"), Paths.get("java-se-04/src/main/java/ava/task1/keyWords"));
+        for (KeywordStatisticsInFile.KeywordCount keywordCount : statistics.getKeywordCountList()) {
+            System.out.printf("%s = %s\n", keywordCount.getKeyword(), keywordCount.getCount());
+        }
+    }
+}
