75c2e24c7f083e679ab28cb37a074851a9fb71f5
Slava Avramenok <_AVA@outlook.com>
2018-04-20 17:29:34 +0300

    08. Task2. Entity book

---
+++ java-se-08/src/main/java/ava/task2/entity/Book.java
@@ -0,0 +1,21 @@
+package ava.task2.entity;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Book {
+    int id;
+    String title;
+    String author;
+    String genre;
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+}
